ba0d8d6627c355c01699d38916fdfda56dc25a78
MehranSarwar1999 <123132426+MehranSarwar1999@users.noreply.github.com>
2023-04-03 23:48:27 -0400

    Add files via upload

---
+++ Summary.java
@@ -0,0 +1,19 @@
+package Concurrency;
+
+class Summary extends Thread {
+	
+	public void run( ) {
+		for(int i=0 ; i<10;i++) {
+			System.out.println(Thread.currentThread().getId() + "Value " + i);				
+		}
+	
+	try {
+	Thread.sleep(100);
+	} catch (InterruptedException e) {
+		//TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+  }
+}
+
+
---
+++ numbers.java
@@ -0,0 +1,13 @@
+package Concurrency;
+
+public class numbers {
+
+	public static void main(String[] args) {
+	    Summary  Summary = new Summary();
+	    Summary.start();
+	    Summary Summary1 = new Summary();
+	    Summary1.start();
+
+	}
+
+}
